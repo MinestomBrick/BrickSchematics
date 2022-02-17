643b7ac2be6e7385555cf328557cc8e6ea0232d9
jorisguffens <joris.guffens@outlook.com>
2022-02-17 15:05:36 +0000

    Deploying to javadocs from @ MinestomBrick/BrickSchematics@a48520c9bf511363b7b794cda6ebe3f1bd6a3bb9 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.minestombrick.schematics.api"},{"l":"org.minestombrick.schematics.api.versions"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"org.minestombrick.schematics.api.versions","l":"AbstractSchematic"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.minestombrick.schematics.api","l":"RegionBlock"},{"p":"org.minestombrick.schematics.api","l":"Schematic"},{"p":"org.minestombrick.schematics.api.versions","l":"SpongeV2Schematic"}];updateSearchResults();
